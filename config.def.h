3362c687bc03958aaf234ccaa45534ec50d9c453
Noah Geller <noah.stanford.geller@gmail.com>
2021-01-12 21:31:39 -0800

    config

--- config.def.h
+++ config.def.h
@@ -6,6 +6,10 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "monospace:pixelsize=14:antialias=true";
+/* Spare fonts */
+static char *font2[] = {
+	"emoji:pixelsize=14:antialias=true",
+};
 static int borderpx = 2;
 
 /*
